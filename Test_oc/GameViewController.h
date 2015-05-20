6b3231fec1c037aad42f4a87892f022737ea9b0d
root <ios@ytxsc.com>
2015-05-20 10:23:43 +0800

    2

---
+++ Test_oc/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Test_oc
+//
+
+//  Copyright (c) 2015年 yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ Test_oc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test_oc
+//
+//  Created by git on 15/5/20.
+//  Copyright (c) 2015年 yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
